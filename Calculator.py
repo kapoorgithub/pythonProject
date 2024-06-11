5305df23916cdd699c836d87536a8c38d92bc113
kapoorgithub <kapoorbgp@gmail.com>
2024-06-11 11:19:49 +0530

    Modified Code

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,9 @@
 # Write a program for calculator
 
-num1 = input("Enter first number: ")
-num2 = input("Enter second number: ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
-print("add=: ", (num1+num2))
+print("add= ", num1+num2)
+print("sub= ", num1-num2)
+print("mult= ", num1*num2)
+print("div= ", num1/num2)
