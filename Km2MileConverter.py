d186ea4cb1af0b57fd3b08e41ed9a065f46c73a3
kapoorgithub <kapoorbgp@gmail.com>
2024-06-13 10:36:11 +0530

    Initial Commit

---
+++ Km2MileConverter.py
@@ -0,0 +1,13 @@
+# Km to Mi conversion in Python
+
+# Input the distance in kilometers from the user
+kilometers = float(input("Enter the distance in kilometers: "))
+
+# Conversion factor for kilometers to miles
+conversion_factor = 0.621371
+
+# Convert kilometers to miles
+miles = kilometers * conversion_factor
+
+# Print the converted distance in miles
+print("The distance in miles is:", miles)
