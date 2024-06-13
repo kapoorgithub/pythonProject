a365d1f47d9cb3a618c25ead03c6115014140013
kapoorgithub <kapoorbgp@gmail.com>
2024-06-13 11:19:00 +0530

    Initial Commit

---
+++ BrandNameGenerator.py
@@ -0,0 +1,6 @@
+# Write a programme to generate Brand Name
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
